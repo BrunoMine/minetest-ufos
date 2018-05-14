522f4caf3b34b768c3dc6336c1a3982246c7b9a2
Lunovox <Lunovox@users.noreply.github.com>
2018-05-14 13:20:04 -0300

    Create components_items.lua

---
+++ ufos/components_items.lua
@@ -0,0 +1,50 @@
+minetest.register_craftitem("ufos:steel_wire", {
+	description = modUFO.translate("Steel Wire"),
+	inventory_image = "obj_steel_wire.png",
+})
+
+minetest.register_craftitem("ufos:steel_spiral", {
+	description = modUFO.translate("Steel Spiral"),
+	inventory_image = "obj_steel_wire.png",
+})
+
+minetest.register_craftitem("ufos:light", {
+	description = modUFO.translate("Light"),
+	inventory_image = "obj_light.png",
+})
+
+minetest.register_craftitem("ufos:gauge", {
+	description = modUFO.translate("Gauge"),
+	inventory_image = "obj_gauge.png",
+})
+
+
+minetest.register_craftitem("ufos:cylinder", {
+	description = modUFO.translate("Cylinder"),
+	inventory_image = "obj_cylinder.png",
+})
+
+minetest.register_craftitem("ufos:steel_pipe", {
+	description = modUFO.translate("Steel Pipe"),
+	inventory_image = "obj_steel_pipe.png",
+})
+
+minetest.register_craftitem("ufos:steel_core", {
+	description = modUFO.translate("Steel Core"),
+	inventory_image = "obj_steel_core.png",
+})
+
+minetest.register_craftitem("ufos:copper_wire", {
+	description = modUFO.translate("Copper Wire"),
+	inventory_image = "obj_copper_wire.png",
+})
+
+minetest.register_craftitem("ufos:copper_coil", {
+	description = modUFO.translate("Copper Coil"),
+	inventory_image = "obj_copper_coil.png",
+})
+
+minetest.register_craftitem("ufos:pump", {
+	description = modUFO.translate("Steel Pump"),
+	inventory_image = "obj_steel_pump.png",
+})
